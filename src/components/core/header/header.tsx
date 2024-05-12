8f2fc01b4cfff2a2e2a98f43ac9d49c7d5db8b36
Pedro Oliveira <pedro.oliveira@qyon.com>
2024-05-12 09:10:09 -0300

    feat: init create modal component (#6)
    * chore: add radix-ui in project
    
    * feat: init create modal component

--- src/components/core/header/header.tsx
+++ src/components/core/header/header.tsx
@@ -1,5 +1,6 @@
 // Packages
 import { ReactElement } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
 
 // Assets
 import LogoImg from "assets/dt-money-logo.svg";
@@ -13,9 +14,22 @@ export const Header = (): ReactElement => {
       <Styled.HeaderContent>
         <img src={LogoImg} alt="Logo marca da DT-MONEY" />
 
-        <Styled.NewTransactionButton>
-          Nova transação
-        </Styled.NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <Styled.NewTransactionButton>
+              Nova transação
+            </Styled.NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </Styled.HeaderContent>
     </Styled.HeaderContainer>
   );
