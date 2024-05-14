2aa3915495f0b9e4972d87020e14713abc88035b
Pedro Oliveira <pedro.oliveira@qyon.com>
2024-05-13 23:59:31 -0300

    feat: create new transaction function in context (#20)

--- src/contexts/transactionsContext.tsx
+++ src/contexts/transactionsContext.tsx
@@ -1,6 +1,8 @@
 // Packages
-import { api } from "lib/axios";
 import { ReactElement, createContext, useEffect, useState } from "react";
+
+// Libs
+import { api } from "lib/axios";
 
 interface Transaction {
   id: number;
@@ -14,6 +16,14 @@ interface Transaction {
 export interface TransactionContextProps {
   transactions: Array<Transaction>;
   fecthTransactions: (query?: string) => Promise<void>;
+  createTransaction: (data: CreateTransactionInput) => Promise<void>;
+}
+
+interface CreateTransactionInput {
+  description: string;
+  price: number;
+  category: string;
+  type: "income" | "outcome";
 }
 
 interface TransactionsProviderProps {
@@ -30,10 +40,29 @@ export const TransactionsProvider = ({
   async function fecthTransactions(query?: string): Promise<void> {
     const response = await api.get(`/transactions`, {
       params: {
+        _sort: "createdAt",
+        _order: "desc",
         q: query,
       },
     });
+
     setTransactions(response?.data);
+  }
+
+  async function createTransaction(
+    data: CreateTransactionInput
+  ): Promise<void> {
+    const { category, description, price, type } = data;
+
+    const response = await api.post("/transactions", {
+      category,
+      description,
+      price,
+      type,
+      createdAt: new Date().toISOString(),
+    });
+
+    setTransactions((state) => [response.data, ...state]);
   }
 
   useEffect(() => {
@@ -41,7 +70,9 @@ export const TransactionsProvider = ({
   }, []);
 
   return (
-    <TransactionsContext.Provider value={{ transactions, fecthTransactions }}>
+    <TransactionsContext.Provider
+      value={{ transactions, fecthTransactions, createTransaction }}
+    >
       {children}
     </TransactionsContext.Provider>
   );
